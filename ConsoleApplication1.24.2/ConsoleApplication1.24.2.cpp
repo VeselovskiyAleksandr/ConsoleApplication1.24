01d7350238aff120c7af7d827a11643aaba100b5
Alexandr <VeselovskiyAlexandr@mail.ru>
2021-11-08 08:00:45 +0300

    tesst 1.24.3

---
+++ ConsoleApplication1.24.2/ConsoleApplication1.24.2.cpp
@@ -0,0 +1,162 @@
+﻿// ConsoleApplication1.24.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Урок 21. Задача 2. Модель данных для посёлка.
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+
+using namespace std;
+
+struct room {
+	string roomType;
+	string roomSquare;
+	
+};
+struct floors {
+	string  quantityRooms;
+	string ceillingHeight;
+
+	struct room rooms;
+};
+
+
+struct cottage {
+	string stove;
+	string  quantityFloor;
+	
+
+	struct floors houseFloor;
+};
+
+struct village {
+	
+	string square, quantityBuilding;
+	string garage, barn, bathhouse;
+
+	struct cottage privateCottage;
+};
+
+
+
+
+int main()
+{
+
+	village plotLand;
+	cottage privateCottage;
+	floors houseFloor1;
+	floors houseFloor2;
+	room rooms1;
+	room rooms2;
+	room rooms3;
+	room rooms4;
+vector < village> plotLands;
+	string str, st, s;
+	vector <floors> houseFloors;
+	vector <room> chamber;
+	fstream file("C:\\Users\\Александр\\Documents\\text for program\\village.txt", ios::app);
+	if (file.is_open()) {
+		cout << "\nThe accounting statement is open for recording.";
+	}
+	else {
+		cerr << "\nThe file is not found. ";
+		return 1;
+	}
+	int action = 0;
+	cout << "\nTo make entry, enter 1; to read the contents enter 2";
+	cin >> action;
+	if (action == 1) {
+		ofstream file("C:\\Users\\Александр\\Documents\\text for program\\village.txt", ios::app);
+	
+		int counter = 0;
+		cout << "\nEnter the number of objects.";
+		cin >> counter;
+		for (int i = 0; i < counter; i++) {
+			cout << "\nEnter the square of  plotLand: ";
+			cin >>plotLand.square;
+			cout << "\n:Enter the number of facility ";
+			cin >> plotLand.quantityBuilding;
+			cout << "\nSpecify whether there is a garage on the plotLand: ";
+			cin >> plotLand.garage;
+			cout << "\n:Specify whether there is a barn on the plotLand ";
+			cin >> plotLand.barn;
+			cout << "\n:Specify whether there is a bathhouse on the plotLand ";
+			cin >> plotLand.bathhouse;
+
+			cout << "\nSpecify whether there is a stove in the house:";
+			cin>> plotLand.privateCottage.stove;
+			cout << "\nSpecify the number of floors: ";
+			cin >> plotLand.privateCottage.quantityFloor;
+             str= plotLand.privateCottage.quantityFloor;
+
+		//	int countF= plotLand.privateCottage.quantityFloor;
+         int countF = stoi(str);
+		
+			for (int j = 0; j < countF; j++) {
+				
+			cout << "\nspecify the number of rooms on the floor: ";
+	          cin>> plotLand.privateCottage.houseFloor.quantityRooms;
+		//cout<< houseFloor.quantityRooms;
+				st= plotLand.privateCottage.houseFloor.quantityRooms;
+			//	int countR= plotLand.privateCottage.houseFloor.quantityRooms;
+				int countR =stoi(st);
+		
+				for (int k = 0; k < countR; k++){
+					cout << "\nspecify the type of rooms: ";
+              cin>> plotLand.privateCottage.houseFloor.rooms.roomType;
+		//	  cout << houseFloor[j].rooms.roomType;
+			  plotLands.push_back(plotLand);
+		
+			//  chamber.push_back(rooms);
+          //   str= plotLand.privateCottage.houseFloor.rooms.roomType;
+           
+					cout << "\nspecify the area of the room: ";
+				cin >> plotLand.privateCottage.houseFloor.rooms.roomSquare;
+					
+				chamber.push_back(rooms[k]);
+				
+
+			//	cout<< chamber[k].roomType<<" ";
+				}
+houseFloors.push_back(houseFloor1);
+
+
+
+				
+			}
+plotLands.push_back(plotLand);
+		}
+		
+		
+		for (int i = 0; i < plotLands.size(); i++) {
+		
+			file << plotLands[i].square << " " << plotLands[i].quantityBuilding << " " << plotLands[i].garage << " " << plotLands[i].barn;
+	//		file <<" "<< plotLands[i].bathhouse<<" "<< plotLand[i].privateCottage.stove; 
+			for (int j = 0; j < houseFloors.size(); j++) {
+				file<< houseFloors[j].quantityRooms<<" "<< houseFloors[j].ceillingHeight<<"\n";
+				for (int k = 0; k < chamber.size(); k++) {
+					file << chamber[k].roomType << " " << chamber[k].roomSquare << "\n";
+				}
+			}
+		}
+	}
+	else if (action == 2) {
+		
+	}
+	file.close();
+	return 0;
+	
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ ConsoleApplication1.24.3/ConsoleApplication1.24.3/ConsoleApplication1.24.3.cpp
@@ -0,0 +1,115 @@
+﻿// ConsoleApplication1.24.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Урок 21. Задача 3.Реализация математического вектора. 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+struct vect {
+	float x, y;
+};
+vect vect1, vect2;
+
+void add(vect vect1,vect vect2) {
+	cout << "Enter the beginning of the first vector";
+	cin >> vect1.x;
+	cout << "Enter the end of the first vector";
+	cin >> vect1.y;
+	cout << "Enter the beginning of the second vector";
+	cin >> vect2.x;
+	cout << "Enter the end of the second vector";
+	cin >> vect2.y;
+	cout << "The sum vector has coordinates: (" << vect1.x + vect2.x << ";" << vect1.y + vect2.y << ")\n";
+}
+
+void subtract(vect vect1, vect vect2) {
+	cout << "Enter the beginning of the first vector";
+	cin >> vect1.x;
+	cout << "Enter the end of the first vector";
+	cin >> vect1.y;
+	cout << "Enter the beginning of the second vector";
+	cin >> vect2.x;
+	cout << "Enter the end of the second vector";
+	cin >> vect2.y;
+	cout<<"The difference vector: vector2- vector1, has coordinates: (" << vect2.x + vect1.x << ";" << vect2.y + vect1.y << ")\n";
+}
+
+void scale(vect vect1) {
+	float k;
+	cout << "\nEnter the scale: ";
+	cin >> k;
+	cout << "Enter the beginning of the  vector";
+	cin >> vect1.x;
+	cout << "Enter the end of the  vector";
+	cin >> vect1.y;
+	cout << "\nMultiplying the vector by the scalar has coordinates: ";
+	cout << "(" << k * vect1.x << ";" << k * vect1.y << ")";
+}
+
+float length(vect vect1) {
+	float length;
+	cout << "Enter the beginning of the  vector";
+	cin >> vect1.x;
+	cout << "Enter the end of the  vector";
+	cin >> vect1.y;
+	length = sqrt(vect1.x * vect1.x + vect1.y * vect1.y);
+	cout << "\nThe length of the vector is " << length;
+	return length;
+}
+
+void normalize(vect vect1) {
+	float length;
+	cout << "Enter the beginning of the  vector";
+	cin>> vect1.x;
+	cout << "Enter the end of the  vector";
+	cin >> vect1.y;
+	length = sqrt(vect1.x * vect1.x + vect1.y * vect1.y);
+//	cout << "\nThe length of the vector " << length;
+	cout << "\nCoordinates of the normalized vector: ";
+	cout << "\nX=" << vect1.x / length << "\n";
+	cout << "Y=" << vect1.y / length;
+}
+
+int main()
+{
+	int n;
+	cout << "\n                 The program for performing action with vectors.";
+	cout << "\n For addition of vectors enter 1";
+	cout << "\n For subtracting of vectors enter 2";
+	cout << "\n For multiplying a vector by a scalar enter 3";	
+	cout << "\n For finding the length of the vector  enter 4";
+    cout << "\n Normalization of the vector enter 5";
+	cout << "\nSelect an action.";
+	cin >> n;
+	if (n == 1) {
+
+		add(vect1, vect2);
+	}
+	else if(n==2){
+		subtract(vect1, vect2);
+	}
+	else if (n == 3) {
+		scale(vect1);
+	}
+	else if (n == 4) {
+		length(vect1);
+	}
+	else if (n == 5) {
+		normalize(vect1);
+	}
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
